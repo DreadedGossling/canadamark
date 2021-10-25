b4e0f2cc215681bd987d85235203ae0b3c2335e7
dr-r3d <abhi@appsbuddha.com>
2021-10-25 18:59:07 +0530

    replace carousal

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,9 @@
     </div>
     <div class="flex flex-col items-center justify-between p-6 text-center my-8">
       <h1 class="max-w-6xl text-cm-blue mb-12 font-sanomat">Our Diamonds</h1>
-      <image-slider />
+            <div class="h-700px flex items-center bg-black">
+        <video src="/videos/preview.mp4" autoplay loop playsinline muted></video>
+      </div>
     </div>
     <div class="max-w-6xl grid grid-cols-2 gap-0 p-6 my-8 mx-auto">
       <div><img class="h-full w-auto" src="/images/engagement.png" alt=""></div>
