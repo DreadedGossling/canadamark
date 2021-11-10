e512c8111d5a3013db328374758c55382579974e
dr-r3d <abhi@appsbuddha.com>
2021-11-10 20:36:29 +0530

    adds arrow to image in step

--- components/Step.vue
+++ components/Step.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="static">
-    <img class="border-4 border-cm-pantone absolute -mt-32 -ml-20 z-10 w-64 hidden" :src="imageUrl" alt="" ref="image"/>
+    <img class="border-4 border-top-cm-pantone absolute -mt-32 -ml-20 z-10 w-64 invisible" :src="imageUrl" alt="" ref="image"/>
+    <div class="arrow ml-9 border-cm-pantone invisible" ref="arrow"></div>
     <img class="border-4 border-cm-pantone rounded-full h-24 w-24" :src="thumbUrl" alt="" @mouseenter="toggleImage" @mouseleave="toggleImage"/>
     <span class="absolute mt-4 text-center" :class="width()">{{ name }}</span>
   </div>  
@@ -22,8 +23,20 @@ export default {
       }
     },
     toggleImage(){
-      this.$refs.image.classList.toggle('hidden')
+      this.$refs.image.classList.toggle('invisible')
+      this.$refs.arrow.classList.toggle('invisible')
     }
   }
 }
 </script>
+<style scoped>
+.arrow{
+  width: 0; 
+  height: 0; 
+  border-width: 10px;
+  border-style: solid;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  border-bottom-width: 0px;
+}
+</style>
