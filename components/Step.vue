47828b6ef51b2b89c22e178407a413fa61d53761
dr-r3d <abhi@appsbuddha.com>
2021-11-10 19:55:31 +0530

    fix step title margins

--- components/Step.vue
+++ components/Step.vue
@@ -2,7 +2,7 @@
   <div class="static">
     <img class="border-4 border-cm-pantone absolute -mt-32 -ml-20 z-10 w-64 hidden" :src="imageUrl" alt="" ref="image"/>
     <img class="border-4 border-cm-pantone rounded-full h-24 w-24" :src="thumbUrl" alt="" @mouseenter="toggleImage" @mouseleave="toggleImage"/>
-    <span class="absolute mt-4 w-24 text-center">{{ name }}</span>
+    <span class="absolute mt-4 text-center" :class="width()">{{ name }}</span>
   </div>  
 </template>
 <script>
@@ -13,6 +13,14 @@ export default {
     thumbUrl: String
   },
   methods: {
+    width() {
+      console.log(this.name)
+      if (this.name.length > 12 ) {
+        return "w-40 -ml-7"
+      }else{
+        return "w-24"
+      }
+    },
     toggleImage(){
       this.$refs.image.classList.toggle('hidden')
     }
