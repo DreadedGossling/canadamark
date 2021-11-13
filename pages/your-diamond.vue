d45c67ec0f1a40b3edb9e44b233a1860f4a1190b
dr-r3d <abhi@appsbuddha.com>
2021-11-13 14:09:15 +0530

    adjust text

--- pages/your-diamond.vue
+++ pages/your-diamond.vue
@@ -8,7 +8,16 @@
       <div class="bg-cm-blue text-white flex flex-col justify-center items-center h-auto py-10 md:py-0 md:h-4/5 w-4/5 md:w-auto m-auto px-12 z-10">
         <h1 class="font-sanomat text-2xl md:text-6xl">Origin</h1>
         <p class="text-center text24 mt-8 mb-4 font-bold font-ddin leading-tight text-sm md:text-2xl">THE NORTHWEST TERRITORIES</p>
-        <p class="text-center text24 font-ddin leading-tight text-sm md:text-2xl">Your diamond comes from the Ekati, Diavik, and Gahcho Kué mines in Canada’s pristine North, about 200 kilometres south of the Arctic circle. With preferential hiring practices and training programs for northern residents, indigenous-led wildlife monitoring programs, and industry-leading water recycling and composting systems, these mines are at the forefront of community engagement, environmental stewardship and waste management practices.</p>
+        <p class="text-center text24 font-ddin leading-tight md:leading-tight text-sm md:text-2xl mb-4">Your diamond comes from the Ekati, Diavik, and Gahcho Kué mines in Canada’s pristine North, about 200 kilometres south of the Arctic circle. With preferential hiring practices and training programs for northern residents, indigenous-led wildlife monitoring programs, and industry-leading water recycling and composting systems, these mines are at the forefront of community engagement, environmental stewardship and waste management practices.</p>
+        <p>
+          <svg class="w-6 md:w-8 inline" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18.123 8.17383L14.123 16.1738L18.123 24.1738" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          <span class="font-ddin text-sm md:text-2xl align-middle">1/3</span>
+          <svg class="w-6 md:w-8 inline transform rotate-180" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18.123 8.17383L14.123 16.1738L18.123 24.1738" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </p>
       </div>
       <div class="bg-origin media h-full w-auto md:-ml-12"></div>
     </div>
@@ -16,16 +25,16 @@
       <div class="h-full w-auto bg-black order-1 md:order-0 media">
         <video class="h-auto md:h-full w-full md:w-auto" src="/videos/preview.mp4" autoplay loop playsinline muted></video>
       </div>
-      <div class="bg-cm-blue text-white flex flex-col justify-center items-center h-auto py-10 md:py-0 h-4/5 w-4/5 md:w-auto m-auto px-12 z-10 order-0 md:order-1 md:-ml-12 z-10">
+      <div class="bg-cm-blue text-white flex flex-col justify-center items-center h-auto py-10 md:py-0 h-4/5 w-4/5 md:w-auto m-auto px-8 z-10 order-0 md:order-1 md:-ml-12 z-10">
         <h1 class="font-sanomat text-2xl md:text-6xl text-center">Define Your</h1>
-        <h1 class="font-sanomat text-2xl md:text-6xl text-center">Own Milestones</h1>
-        <p class="text-center text24 font-ddin leading-tight text-sm md:text-2xl">True luxury is feeling good about your choices.</p>
+        <h1 class="font-sanomat text-2xl md:text-6xl text-center mb-4">Own Milestones</h1>
+        <p class="text-center text24 font-ddin leading-tight md:leading-tight text-sm md:text-2xl">True luxury is feeling good about your choices.</p>
       </div>
     </div>    
     <div class="max-w-6xl grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-0 py-0 md:p-6 mt-6 md:my-8 mx-auto h-700px two-section p-6">
       <div class="bg-cm-blue text-white flex flex-col justify-center items-center h-auto py-10 md:py-0 md:h-4/5 w-4/5 md:w-auto m-auto px-12 z-10">
-        <h1 class="font-sanomat text-2xl md:text-6xl">Bridal</h1>
-        <p class="text-center text24 font-ddin leading-tight text-sm md:text-2xl">We ensure that our diamonds meet specific standards for color, clarity, finish, and symmetry. All you have to do is pick the right one for you.</p>
+        <h1 class="font-sanomat text-2xl md:text-6xl mb-4">Bridal</h1>
+        <p class="text-center text24 font-ddin leading-tight md:leading-tight text-sm md:text-2xl">We ensure that our diamonds meet specific standards for color, clarity, finish, and symmetry. All you have to do is pick the right one for you.</p>
       </div>
       <div class="bg-bridal media h-full w-auto md:-ml-12"></div>
     </div>
