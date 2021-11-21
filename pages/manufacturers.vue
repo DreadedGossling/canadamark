a86b9f1e5b0a46bdd7a8bf6b4df4060523ac3ab1
dr-r3d <abhi@appsbuddha.com>
2021-11-21 13:51:01 +0530

    updates manufacutres

--- pages/manufacturers.vue
+++ pages/manufacturers.vue
@@ -8,15 +8,14 @@
       <h1 class="font-sanomat text-cm-blue text-center text-2xl md:text-6xl">Canadamark Manufacturers</h1>
       <div class="font-ddin leading-tight text-center flex my-10 text-xs sm:text-base md:text-xl">
         <a class="p-1 sm:p-4 ml-auto cursor-pointer" :class="jewelleryTabClass" target="_blank" @click.prevent="activate('jewellery')">Jewellery Manufacturers</a>
-        <a class="p-1 sm:p-4 cursor-pointer" :class="diamondTabClass" target="_blank" @click="activate('diamond')">Diamond Manufacturers</a>
-        <div class="p-1 sm:p-4 cursor-pointer" :class="polishersTabClass" target="_blank" @click="activate('polishers')">Certified Polishers</div>
-        <div class="p-1 sm:p-4 mr-auto cursor-pointer" :class="stockistsTabClass" target="_blank" @click="activate('stockists')">Stockists</div>
+        <a class="p-1 sm:p-4 cursor-pointer" :class="diamondTabClass" target="_blank" @click="activate('melee')">Melee Manufacturers</a>
+        <div class="p-1 sm:p-4 mr-auto cursor-pointer" :class="polishersTabClass" target="_blank" @click="activate('polishers')">Certified Polishers</div>
       </div>
       <div class="grid grid-col-1 md:grid-cols-2 font-ddin leading-tight gap-y-4 px-6">
         <div v-for="item in activeTabData" :key="item.name">
           <a class="text-base md:text-2xl font-black" :href="item.url">{{ item.name }}</a>
           <div class="text-sm md:text-lg" v-for="email in item.emails" :key="email">{{ email }}</div>
-          <div class="text-sm md:text-lg">{{ item.phone }}</div>
+          <div class="text-sm md:text-lg" v-for="phone in item.phones" :key="phone">{{ phone }}</div>
         </div>
       </div>
     </div>
@@ -46,31 +45,27 @@
 export default {
   data(){
     const jewellery = [
-      {name: 'Mahendra Brothers Exports Pvt. Ltd / Unidesign', emails: ['rishi@mahendrabrothers.com'], url: 'https://mahendrabrothers.com', phone: '+91 22 6661 5600'},
-      {name: 'Corona Jewellery Company Ltd.', emails: ['mike@coronajewellery.com'], url: 'http://www.coronajewellery.com/', phone: '+1 416 7622222'}]
-    const diamond = [
-      {name: 'Singh Diamonds', emails: ['karan@singhdiamonds.com', 'info@singhdiamonds.com'], url: 'http://www.singhdiamonds.com/', phone: '+91 22 26754675'},
-      {name: 'Star Rays', emails: ['rahul@starrays.com'], url: 'http://starrays.com', phone: '+91 261 255 4444'},
-      {name: 'Kapu Gems Llp', emails: ['kalpesh@kapugems.com'], url: 'http://www.kapugems.com/', phone: '+91 22 4311 2233'},
-      {name: 'Finestar Jewellery and Diamonds Pvt. Ltd.', emails: ['nilesh@finestardiamonds.com', 'elza@finestardiamonds.com'], url: 'http://www.finestardiamonds.com', phone: '+91 22 6663 9999'},
-      {name: 'Hari Krishna Exports Pvt. Ltd.', emails: ['bhavik.bhalala@hk.co', 'gd@hk.co'], url: 'http://www.hk.co', phone: '+91 22 2367 8111'},
-      {name: 'Shivani Gems Inc.', emails: ['mayur@shivanigems.com'], url: 'http://www.shivanigems.com/', phone: '+1-212-593-2750'}]
+      {name: 'Mahendra Brothers Exports Pvt. Ltd / Unidesign', emails: ['rishi@mahendrabrothers.com'], url: 'https://mahendrabrothers.com', phones: ['+91 22 6661 5600']},
+      {name: 'Corona Jewellery Company Ltd.', emails: ['mike@coronajewellery.com'], url: 'http://www.coronajewellery.com/', phones: ['+1 416 7622222']}]
+    const melee = [
+      {name: 'Mahendra Brothers Exports Pvt. Ltd', emails: ['rishi@mahendrabrothers.com'], url: 'https://mahendrabrothers.com', phones: ['+91 22 6661 5600']},
+      {name: 'C.J.Exporters India', emails: ['mitul@cjexporters.com', 'samirntr@yahoo.com'], url: 'http://www.singhdiamonds.com/', phones: ['+91 9820023514', '+1 917 208-7701']}]
     const polishers = [
-      {name: 'Finestar Jewellery and Diamonds Pvt. Ltd.', emails: ['nilesh@finestardiamonds.com', 'elza@finestardiamonds.com'], url: 'http://www.finestardiamonds.com', phone: '+91 22 6663 9999'},
-      {name: 'Hari Krishna Exports Pvt. Ltd.', emails: ['bhavik.bhalala@hk.co', 'gd@hk.co'], url: 'http://www.hk.co', phone: '+91 22 2367 8111'},
-      {name: 'Singh Diamonds', emails: ['karan@singhdiamonds.com', 'info@singhdiamonds.com'], url: 'http://www.singhdiamonds.com/', phone: '+91 22 26754675'},
-      {name: 'Shree Ramkrishna Exports Pvt. Ltd.', emails: ['chirag.rc@srk.me'], url: 'https://srk.one/', phone: '+91 261 4040 555'}]
-    const stockists = [{name: 'Corona Jewellery Company Ltd.', emails: ['mike@coronajewellery.com'], url: 'http://www.coronajewellery.com', phone: '+1 416 7622222'}]
-    const retailers = []
-
+      {name: 'Corona Jewellery Company Ltd.', emails: ['mike@coronajewellery.com'], url: 'http://www.coronajewellery.com/', phones: ['+1 416 7622222']},
+      {name: 'Finestar Jewellery and Diamonds Pvt. Ltd.', emails: ['nilesh@finestardiamonds.com', 'elza@finestardiamonds.com'], url: 'http://www.finestardiamonds.com', phones: ['+91 22 6663 9999']},
+      {name: 'Hari Krishna Exports Pvt. Ltd.', emails: ['bhavik.bhalala@hk.co', 'gd@hk.co'], url: 'http://www.hk.co', phones: ['+91 22 2367 8111']},
+      {name: 'Kapu Gems Llp', emails: ['kalpesh@kapugems.com'], url: 'http://www.kapugems.com/', phones: ['+91 22 4311 2233']},
+      {name: 'Shivani Gems Inc.', emails: ['mayur@shivanigems.com'], url: 'http://www.shivanigems.com/', phones: ['+1-212-593-2750']},
+      {name: 'Shree Ramkrishna Exports Pvt. Ltd.', emails: ['chirag.rc@srk.me'], url: 'https://srk.one/', phones: ['+91 261 4040 555']},
+      {name: 'Singh Diamonds', emails: ['karan@singhdiamonds.com', 'info@singhdiamonds.com'], url: 'http://www.singhdiamonds.com/', phones: ['+91 22 26754675']},
+      {name: 'Star Rays', emails: ['rahul@starrays.com'], url: 'http://starrays.com', phones: ['+91 261 255 4444']},
+      {name: 'Venus Jewels', emails: ['akshay@venusjewel.com'], url: 'http://www.venusjewel.com/', phones: ['+91 22 2672 0000']},]
     return {
       activeTab: 'jewellery',
       tabContent: {
         jewellery,
-        diamond,
+        melee,
         polishers,
-        stockists,
-        retailers
       }
     }
   },
@@ -92,16 +87,10 @@ export default {
       return this.calculateTabClass('jewellery')
     },
     diamondTabClass(){
-      return this.calculateTabClass('diamond')
+      return this.calculateTabClass('melee')
     },
     polishersTabClass(){
       return this.calculateTabClass('polishers')
-    },
-    stockistsTabClass(){
-      return this.calculateTabClass('stockists')
-    },
-    retailersTabClass(){
-      return this.calculateTabClass('retailers')
     },
     activeTabData(){
       return this.tabContent[this.activeTab]
